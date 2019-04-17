370f8c3b4c0dddabfa6b23bff8b199f0c0dbf4b2
gcaule <germain.caule@gmail.com>
2019-04-17 07:58:26 +0200

    Fixed indentation

--- src/application/PasswordPage.java
+++ src/application/PasswordPage.java
@@ -13,7 +13,7 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class PasswordPage extends Application{
-	
+
 	public static void main(String[] args) {
 
 		launch(args);
@@ -22,45 +22,45 @@ public class PasswordPage extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-	
-	AnchorPane anchorpane = new AnchorPane();
-	
-	GridPane root = new GridPane();
 
-    Label labelUserName = new Label("Login");
-    TextField fieldUserName = new TextField();
+		AnchorPane anchorpane = new AnchorPane();
 
-    Label labelPassword = new Label("Password");
-    PasswordField fieldPassword = new PasswordField();
-    
-    root.add(labelUserName, 0, 0);
-    root.add(fieldUserName, 1, 0);
-    
-    root.add(labelPassword, 0, 1);
-    root.add(fieldPassword, 1, 1);
-    
-    HBox hbox = new HBox();
-    
-    Button cancelButton = new Button("Cancel");
-    Button enterButton = new Button("OK");
-    
-    ObservableList list = hbox.getChildren();
-    list.addAll(cancelButton, enterButton);
-    
-    root.add(hbox, 1, 2);
-    
-	anchorpane.getChildren().addAll(root);
-	
-	anchorpane.setTopAnchor(root, 15.0);
-	anchorpane.setBottomAnchor(root, 15.0);
-	anchorpane.setLeftAnchor(root, 15.0);
-	anchorpane.setRightAnchor(root, 15.0);
-	
-	Scene scene = new Scene(anchorpane, 337, 136);
+		GridPane root = new GridPane();
 
-	primaryStage.setScene(scene);
-	primaryStage.show();
-	
+		Label labelUserName = new Label("Login");
+		TextField fieldUserName = new TextField();
+
+		Label labelPassword = new Label("Password");
+		PasswordField fieldPassword = new PasswordField();
+
+		root.add(labelUserName, 0, 0);
+		root.add(fieldUserName, 1, 0);
+
+		root.add(labelPassword, 0, 1);
+		root.add(fieldPassword, 1, 1);
+
+		HBox hbox = new HBox();
+
+		Button cancelButton = new Button("Cancel");
+		Button enterButton = new Button("OK");
+
+		ObservableList list = hbox.getChildren();
+		list.addAll(cancelButton, enterButton);
+
+		root.add(hbox, 1, 2);
+
+		anchorpane.getChildren().addAll(root);
+
+		anchorpane.setTopAnchor(root, 15.0);
+		anchorpane.setBottomAnchor(root, 15.0);
+		anchorpane.setLeftAnchor(root, 15.0);
+		anchorpane.setRightAnchor(root, 15.0);
+
+		Scene scene = new Scene(anchorpane, 337, 136);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
 	}
 
 }
